b7f6d89ce392f0a9a887b7c365c2e4da8fd4d2b0
qilei123 <qileimail123@gmail.com>
2018-07-13 11:34:15 -0400

    fsdfa

--- data/openimagev4/create_data.sh
+++ data/openimagev4/create_data.sh
@@ -4,8 +4,8 @@ root_dir=$cur_dir/../..
 cd $root_dir
 
 redo=1
-data_root_dir="$HOME/data/VOCdevkit/"
-dataset_name="VOC0712Plus"
+data_root_dir="./data/VOCdevkit/"
+dataset_name="VOC0712"
 mapfile="$root_dir/data/$dataset_name/labelmap_voc.prototxt"
 anno_type="detection"
 db="lmdb"
@@ -19,7 +19,7 @@ if [ $redo ]
 then
   extra_cmd="$extra_cmd --redo"
 fi
-for subset in trainval
+for subset in trainval test
 do
   python $root_dir/scripts/create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $root_dir/data/$dataset_name/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
 done
