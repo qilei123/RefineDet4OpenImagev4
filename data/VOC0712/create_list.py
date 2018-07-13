4c853e277773d1943b86baa3f8247d6c6f3edb96
qilei123 <qileimail123@gmail.com>
2018-07-13 14:18:59 -0400

    sfda

--- data/VOC0712/create_list.py
+++ data/VOC0712/create_list.py
@@ -15,7 +15,7 @@ train_names_file = open(train_names_dir)
 trainval_file = open('trainval.txt','wb')
 line = train_names_file.readline()
 while line:
-    line_n = train_annos_dir+'/'+line[:len(line)-1]+'.jpg'+' '+train_annos_dir+'/'+line[:len(line)-1]+'.xml\n'
+    line_n = train_images_dir+'/'+line[:len(line)-1]+'.jpg'+' '+train_annos_dir+'/'+line[:len(line)-1]+'.xml\n'
     trainval_file.write(line_n);
     line = train_names_file.readline()
 trainval_file.close()
@@ -26,10 +26,11 @@ test_file = open('test.txt','wb')
 test_name_size_file = open('test_name_size.txt','wb')
 line  = test_names_file.readline()
 while line:
-    line_n = test_annos_dir+'/'+line[:len(line)-1]+'.jpg'+' '+test_annos_dir+'/'+line[:len(line)-1]+'.xml\n'
+    line_n = test_images_dir+'/'+line[:len(line)-1]+'.jpg'+' '+test_annos_dir+'/'+line[:len(line)-1]+'.xml\n'
     test_file.write(line_n)
     image = cv2.imread(test_annos_dir+'/'+line[:len(line)-1]+'.jpg')
-    line_m = line[:len(line)-1]+' '+image.shape[0]+' '+image.shape[1]+'\n'
+    size = image.shape
+    line_m = line[:len(line)-1]+' '+str(size[0])+' '+str(size[1])+'\n'
     test_name_size_file.write(line_m)
     line = test_names_file.readline()
 test_file.close()
